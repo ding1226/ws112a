a1e781da4b4d4672a71228b8c4dffb589df93a7e
ding1226 <125621604+ding1226@users.noreply.github.com>
2023-10-13 18:27:04 +0800

    Update 01.js

--- 01.js
+++ 01.js
@@ -2,30 +2,30 @@ import{ Application, send} from "https://deno.land/x/oak/mod.ts";
 
 const app = new Application();
 
-app.use(async (ctx) => {
+app.use(async(ctx) => {
   console.log(`path=${ctx.request.url.pathname}`)
   ctx.response.body =`
     <html>
          <body>
          </body>
     </html>`
- if (ctx.request.url.pathname==`/nqu`){
+ if (ctx.request.url.pathname==`/nqu/`){
     ctx.response.body = `
     <html>
         <body>
-            <a href="https://www.nqu.edu.tw/">NQU</a>
+            <a href="https://www.nqu.edu.tw/">金門大學</a>
         </body>
     </html>`
  }
- else if(ctx.request.url.pathname==`/nqu/csie`){
+ else if(ctx.request.url.pathname==`/nqu/csie/`){
   ctx.response.body =`
     <html>
-         </body>
-              <a href="https://csie.nqu.edu.tw/">NQU_CSIE</a>
          <body>
+              <a href="https://csie.nqu.edu.tw/">金門大學資工系</a>
+         </body>
     </html>`
  }
- else if(ctx.request.url.pathname==`/to/nqu`){
+ else if(ctx.request.url.pathname==`/to/nqu/`){
   ctx.response.redirect('https://www.nqu.edu.tw/')
  }
  else if(ctx.request.url.pathname==`/to/nqu/csie`){
@@ -34,7 +34,3 @@ app.use(async (ctx) => {
  
  console.log('start at : http://127.0.0.1:8000')
  await app.listen({ port: 8000 });
-});
-
-console.log('Server started at: http://127.0.0.1:8000');
-await app.listen({ port: 8000 });
