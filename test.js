9cdf6f2a914da3f097462a4276764f33bb1b3205
ding1226 <125621604+ding1226@users.noreply.github.com>
2024-01-12 03:25:43 +0800

    Update serve.js

--- test.js
+++ test.js
@@ -1,34 +1,45 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
 
-mongoose.connect('your-database-uri', { useNewUrlParser: true, useUnifiedTopology: true });
-
-const Post = mongoose.model('Post', {
-  title: String,
-  content: String,
-});
-
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.get('/', async (req, res) => {
-  const posts = await Post.find();
-  res.render('index', { posts });
+let courses = [];
+let users = [];
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/courses', (req, res) => {
+  res.json(courses);
+});
 
-app.post('/post', async (req, res) => {
-  const { title, content } = req.body;
-  const post = new Post({ title, content });
-  await post.save();
-  res.redirect('/');
+app.post('/courses', (req, res) => {
+  const { title, instructor, description } = req.body;
+  const course = { id: courses.length, title, instructor, description };
+  courses.push(course);
+  res.json(course);
+});
+
+app.get('/users', (req, res) => {
+  res.json(users);
+});
+
+app.post('/users', (req, res) => {
+  const { username, email, password } = req.body;
+  const user = { id: users.length, username, email, password };
+  users.push(user);
+  res.json(user);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
 });
 
 app.listen(port, () => {
